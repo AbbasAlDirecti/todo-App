ba194dc44f6bd8076c33e46b7dc34616bf740b09
Abbas Ali <abbas.al@media.net>
2020-07-21 16:55:14 +0530

    Added eventHandlers

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,1 +1,26 @@
 // put js for todo app here
+
+function addTodo(event){
+    console.log(event.target);
+    event.preventDefault();
+    //console.log(event);
+    var todoText = document.querySelector('#todo');
+    if(todoText.value == ""){
+        return ;
+    }
+
+    var todoList = document.querySelector('.list');
+    var todoItem = document.createElement('li')
+    todoItem.innerText = todoText.value;
+    todoList.appendChild(todoItem);
+    todoText.value = "";
+}
+
+function deleteTodo(event){
+    console.log(event.target);
+    event.target.parentNode.removeChild(event.target);
+}
+
+var listItems = document.querySelectorAll('li');
+for(var l of listItems)
+  l.addEventListener('click',deleteTodo);
